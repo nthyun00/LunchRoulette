215ba91938e6573fff1c9ae009fb6c5001eaab6a
Ryu <nthyun00@naver.com>
2018-08-20 02:21:06 +0900

    First Commit

---
+++ LunchRoulette/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace LunchRoulette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.새로만들기NToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.다른이름으로저장AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일FToolStripMenuItem
+            // 
+            this.파일FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.새로만들기NToolStripMenuItem,
+            this.열기OToolStripMenuItem,
+            this.저장SToolStripMenuItem,
+            this.다른이름으로저장AToolStripMenuItem});
+            this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
+            this.파일FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.파일FToolStripMenuItem.Text = "파일(&F)";
+            // 
+            // 새로만들기NToolStripMenuItem
+            // 
+            this.새로만들기NToolStripMenuItem.Name = "새로만들기NToolStripMenuItem";
+            this.새로만들기NToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.새로만들기NToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.새로만들기NToolStripMenuItem.Text = "새로 만들기(&N)";
+            this.새로만들기NToolStripMenuItem.Click += new System.EventHandler(this.새로만들기NToolStripMenuItem_Click);
+            // 
+            // 열기OToolStripMenuItem
+            // 
+            this.열기OToolStripMenuItem.Name = "열기OToolStripMenuItem";
+            this.열기OToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.열기OToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.열기OToolStripMenuItem.Text = "열기(&O)";
+            this.열기OToolStripMenuItem.Click += new System.EventHandler(this.열기OToolStripMenuItem_Click);
+            // 
+            // 저장SToolStripMenuItem
+            // 
+            this.저장SToolStripMenuItem.Name = "저장SToolStripMenuItem";
+            this.저장SToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.저장SToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.저장SToolStripMenuItem.Text = "저장(&S)";
+            this.저장SToolStripMenuItem.Click += new System.EventHandler(this.저장SToolStripMenuItem_Click);
+            // 
+            // 다른이름으로저장AToolStripMenuItem
+            // 
+            this.다른이름으로저장AToolStripMenuItem.Name = "다른이름으로저장AToolStripMenuItem";
+            this.다른이름으로저장AToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.다른이름으로저장AToolStripMenuItem.Text = "다른 이름으로 저장(&A)";
+            this.다른이름으로저장AToolStripMenuItem.Click += new System.EventHandler(this.다른이름으로저장AToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(200, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "뽑기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 400);
+            this.listBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(175, 348);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(200, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(200, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(300, 500);
+            this.Name = "Form1";
+            this.Text = "제목 없음 - LunchRoulette";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 새로만들기NToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 열기OToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 다른이름으로저장AToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LunchRoulette/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LunchRoulette
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private String path = "";
+        private Boolean isChanged = false;
+        
+        private void Save()
+        {
+            if (this.path.Length == 0)
+                throw new Exception("No File Linked");
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(this.path, false, Encoding.Default))
+                {
+                    foreach (var tmp in listBox1.Items)
+                    {
+                        sw.WriteLine(tmp);
+                    }
+                }
+                isChanged = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+
+        }
+
+        private void SaveAS()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Title = "다른 이름으로 저장",
+                Filter = "텍스트 문서(*.txt)|*.txt"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.path = saveFileDialog.FileName;
+                this.Text = this.path.Split('\\')[this.path.Split('\\').Length - 1] + " - LunchRoulette";
+                this.Save();
+            }
+        }
+
+        private void Open(String path)
+        {
+            this.path = path;
+            this.Text = path.Split('\\')[path.Split('\\').Length - 1] + " - LunchRoulette";
+            listBox1.Items.Clear();
+            try
+            {
+                using (StreamReader sr = new StreamReader(this.path, Encoding.Default))
+                {
+                    String tmp;
+                    while ((tmp = sr.ReadLine()) != null)
+                    {
+                        if (tmp.Length == 0)
+                            continue;
+                        listBox1.Items.Add(tmp);
+                    }
+                }
+                isChanged = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        private void OpenDialog()
+        {
+            try
+            {
+                if (this.SaveCheck() == DialogResult.Cancel)
+                    return;
+
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "열기",
+                Filter = "텍스트 문서(*.txt)|*.txt"
+
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.Open(openFileDialog.FileName);
+            }
+        }
+
+        private void ChooseRandomPick()
+        {
+            try
+            {
+                label1.Text = listBox1.Items[(new Random().Next()) % listBox1.Items.Count].ToString();
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+
+        private void AddItems(String text)
+        {
+            if (text.Length == 0)
+                return;
+            listBox1.Items.Add(text);
+            textBox1.Text = "";
+            isChanged = true;
+        }
+
+        private void DeleteItems(Object value)
+        {
+            listBox1.Items.Remove(value);
+            isChanged = true;
+            
+        }
+
+        private DialogResult SaveCheck()
+        {
+            if (isChanged == false)
+                throw new Exception();
+            DialogResult dr = MessageBox.Show(("변경 내용을 " + (this.path.Length == 0 ? "제목 없음" : this.path) + "에 저장하시겠습니까?"), "LunchRoulette", MessageBoxButtons.YesNoCancel);
+           
+            if(dr==DialogResult.Yes)
+            {
+                try
+                {
+                    this.Save();
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message == "No File Linked")
+                        this.SaveAS();
+                }
+            }
+
+            return dr;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            for (int i = 1; i < 100; i++) 
+            {
+
+                this.ChooseRandomPick();
+                Application.DoEvents();
+                Thread.Sleep(i);
+            }
+            label1.Text = "";
+            Application.DoEvents();
+            Thread.Sleep(500);
+            MessageBox.Show(listBox1.Items[(new Random().Next()) % listBox1.Items.Count].ToString(), "Result");
+            button1.Visible = true;
+        }
+
+        private void 열기OToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.OpenDialog();
+        }
+
+        private void 다른이름으로저장AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.SaveAS();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.AddItems(textBox1.Text);
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Save();
+            }
+            catch(Exception ex)
+            {
+                if (ex.Message == "No File Linked")
+                    this.SaveAS();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (this.SaveCheck() == DialogResult.Cancel)
+                    e.Cancel = true;
+                
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void 새로만들기NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1_FormClosing(sender, null);
+            Form1_Load(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.path = "";
+            this.isChanged = false;
+            this.Text = "제목 없음 - LunchRoulette";
+            listBox1.Items.Clear();
+            label1.Text = "";
+            textBox1.Text = "";
+            Form1_SizeChanged(null, null);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.DeleteItems(listBox1.Items[listBox1.SelectedIndex]);
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                try
+                {
+                    if (this.SaveCheck() == DialogResult.Cancel)
+                        return;
+
+                }
+                catch (Exception)
+                {
+
+                }
+
+                this.Open(((String[])e.Data.GetData(DataFormats.FileDrop))[0]);
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy | DragDropEffects.Scroll;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            listBox1.Height = this.Height - 100;
+            listBox1.Width = this.Width - 150;
+            label1.Location = new Point(listBox1.Width + 25, label1.Location.Y);
+            button1.Location = new Point(listBox1.Width + 50, button1.Location.Y);
+            button2.Location = new Point(listBox1.Width + 50, button2.Location.Y);
+            button3.Location = new Point(listBox1.Width + 50, button3.Location.Y);
+            textBox1.Location = new Point(listBox1.Width + 25, textBox1.Location.Y);
+        }
+    }
+}
